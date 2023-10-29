3a099ae3f0ae3f9cfa3261c24d1699ccc462e37d
Sandros94 <sandro.circi@digitoolmedia.com>
2023-10-29 20:50:10 +0100

    feat: define model via query for new chat

--- pages/chats/[chat-id].vue
+++ pages/chats/[chat-id].vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-full min-h-screen overflow-y-auto">
-    <UiChat :chat="chatHistory" :chat-id="(chatid as string)" :page-title="(pageTitle as string)" />
+    <UiChat :chat="chatHistory" :chat-id="(chatid as string)" :page-title="(pageTitle as string)" :model="(aiModel as string)" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Chat } from '~/types'
 
-const { params: { chatid }, query: { title: pageTitle } } = useRoute()
+const { params: { chatid }, query: { title: pageTitle, model: aiModel } } = useRoute()
 
 const { data: chatHistory } = await useFetch<Chat>('/api/chats/readSingle', {
   key: `chat-${chatid}`,
