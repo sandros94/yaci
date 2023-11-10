947ec72e52197f0b2898868230244c6040694f47
Sandros94 <sandro.circi@digitoolmedia.com>
2023-11-10 18:15:11 +0100

    fix: prevent posting chats without version & title

--- server/api/chats/index.post.ts
+++ server/api/chats/index.post.ts
@@ -5,6 +5,10 @@ export default defineEventHandler(async (event) => {
 
   if (!body.id) {
     throw new Error('Missing chat ID')
+  } else if (!body.yaci.version) {
+    throw new Error('Missing chat version')
+  } else if (!body.settings.title) {
+    throw new Error('Missing chat title')
   }
   await useStorage('chats').setItem<Chat>(body.id, body)
 })
